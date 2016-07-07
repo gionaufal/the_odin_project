e3e29f05ad74baf3dbfad8029a311211c0ea7a27
gionaufal <g10v45+github@gmail.com>
2016-07-07 09:32:52 -0300

    update js-jquery exercise

---
+++ js-jquery/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  var grid = 16;
+  for(var x = 0; x < grid; x++) {
+        for(var y = 0; y < grid; y++) {
+          var $div = $("<div class='empty'></div>");
+          $div.width(600/grid);
+          $div.height(600/grid)
+          $div.appendTo('.wrapper');
+        }
+  }
+
+  $('.empty').hover(function(){
+    $(this).addClass('hover')
+  });
+
+
+})
