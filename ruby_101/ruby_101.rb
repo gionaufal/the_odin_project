7e3e6030d22bfa934cae871652ddddfdf50a258e
gionaufal <g10v45+github@gmail.com>
2016-07-10 22:10:38 -0300

    add ruby_101 exercises

---
+++ ruby_101/ruby_101.rb
@@ -0,0 +1,22 @@
+time_machine = true
+
+# Variables in Ruby souldn't have their description (count, integer, etc) in their name
+student_count_integer = 0
+
+# Nor sould be in camelCase
+homeworkAssignment = 0
+
+# They can't start with numbers
+3_sections = false
+
+# And souldn't be abreviated
+top_ppl = 0
+
+numbers = "one,two,three,four,five"
+
+numbers.split(",")
+
+string.sub
+string.gsub #global
+
+"this is a sentence".gsub("e"){ puts "Found an E!"}
